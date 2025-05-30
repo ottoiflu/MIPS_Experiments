7516b65aa7eb6b56f2cfd52922981deead507b7a
YueKang <2389080918@qq.com>
2025-05-30 16:42:25 +0800

    修复 .gitignore，使部分文件被保留

--- 涂增基报告/MIPS Microcomputer Experiment/Adder.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-module Adder (In1,In2,Out);  
-//加法器 Adder 将两个 n 位宽的数据相加，输出结果
-    parameter n=32;
-input [n-1:0] In1,In2;
-output [n-1:0] Out;
-assign Out =In1 +In2;
-
-endmodule
--- 涂增基报告/MIPS Microcomputer Experiment/Mux2_1.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-module Mux2_1 (In1, In2, sel, Out);  //多路复用器 Mux2_1
-parameter n=32;
-input [n-1:0] In1, In2;   //输入：In1（n 位），In2（n 位），sel
-input sel;
-output [n-1:0] Out;    //输出：Out（n 位）
-//将两个 n 位宽的输入通道复用到一个输出同位宽通道，当 sel 为 0 时，选择 In1 输出，否则选择 In2 输出
-assign Out = sel?In2:In1;
-endmodule
